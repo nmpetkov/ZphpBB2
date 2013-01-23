cd7ba91600f22996b0b607275b131bb83c10caac
N. Petkov <nmpetkov@gmail.com>
2013-01-23 19:34:23 +0200

    Tipo

--- modules/ZphpBB2/pnblocks/Lastposts.php
+++ modules/ZphpBB2/pnblocks/Lastposts.php
@@ -25,63 +25,63 @@ function ZphpBB2_Lastpostsblock_info()
 
 function ZphpBB2_Lastpostsblock_display($blockinfo)
 {
-	if (!SecurityUtil::checkPermission('ZphpBB2_Lastposts::', $blockinfo[bid]."::", ACCESS_READ)) {
-   		return;
-	}
+    if (!SecurityUtil::checkPermission('ZphpBB2_Lastposts::', $blockinfo[bid]."::", ACCESS_READ)) {
+        return;
+    }
 
-	global $bgcolor1, $bgcolor2, $bgcolor3, $bgcolor4, $bgcolor5, $textcolor1, $textcolor2, $textcolor3, $textcolor4;
+    global $bgcolor1, $bgcolor2, $bgcolor3, $bgcolor4, $bgcolor5, $textcolor1, $textcolor2, $textcolor3, $textcolor4;
 
-	// get module information
-	$modinfo = ModUtil::getInfoFromName("ZphpBB2");
-	if (empty($modinfo['name']) or empty($modinfo['directory'])) {
-		return _MODULE_ERROR;
-	}
+    // get module information
+    $modinfo = ModUtil::getInfoFromName("ZphpBB2");
+    if (empty($modinfo['name']) or empty($modinfo['directory'])) {
+        return _MODULE_ERROR;
+    }
 
-	// Get variables from content block
-	$vars = BlockUtil::varsFromContent($blockinfo['content']);
-	$table_prefix = System::getVar('prefix') . "_phpbb_";
+    // Get variables from content block
+    $vars = BlockUtil::varsFromContent($blockinfo['content']);
+    $table_prefix = System::getVar('prefix') . "_phpbb_";
 
-	define('IN_PHPBB', true);
-	// Implementation cached content
-	$enable_cache = true;
-	$write_to_cache = false;	# flag
-	$cache_time = 180; # seconds
-	if (isset($vars['cache_time'])) $cache_time = $vars['cache_time'];
-	$content = "";
-	if ($enable_cache and $cache_time>0) {
-		$cachefilestem = 'ZphpBB2_' . $blockinfo['bid'];
-	    $cachedir = System::getVar('temp');
-	    if (StringUtil::right($cachedir, 1)<>'/') $cachedir .= '/';
-	    if (isset($vars['cache_dir']) and !empty($vars['cache_dir'])) $cachedir .= $vars['cache_dir'];
-	    else $cachedir .= 'any_cache';
-	    $cachefile = $cachedir .'/'. $cachefilestem;
-		// attempt to load from cache
-		if (file_exists($cachefile)) {
-			$file_time = filectime($cachefile);
-			$now = time();
-			$diff = ($now - $file_time);
-			if ($diff <= $cache_time) {
-			    $content = file_get_contents($cachefile);
-			}
-		}
-		if (empty($content)) $write_to_cache = true; # not loaded, flag to write to cache later
-	}
-	if (empty($content)) {
-		// Create output object
+    define('IN_PHPBB', true);
+    // Implementation cached content
+    $enable_cache = true;
+    $write_to_cache = false;	# flag
+    $cache_time = 180; # seconds
+    if (isset($vars['cache_time'])) $cache_time = $vars['cache_time'];
+    $content = "";
+    if ($enable_cache and $cache_time>0) {
+        $cachefilestem = 'ZphpBB2_' . $blockinfo['bid'];
+        $cachedir = System::getVar('temp');
+        if (StringUtil::right($cachedir, 1)<>'/') $cachedir .= '/';
+        if (isset($vars['cache_dir']) and !empty($vars['cache_dir'])) $cachedir .= $vars['cache_dir'];
+        else $cachedir .= 'any_cache';
+        $cachefile = $cachedir .'/'. $cachefilestem;
+        // attempt to load from cache
+        if (file_exists($cachefile)) {
+            $file_time = filectime($cachefile);
+            $now = time();
+            $diff = ($now - $file_time);
+            if ($diff <= $cache_time) {
+                $content = file_get_contents($cachefile);
+            }
+        }
+        if (empty($content)) $write_to_cache = true; # not loaded, flag to write to cache later
+    }
+    if (empty($content)) {
+        // Create output object
 
-		// include some files
-		$phpbb_root_path = "./modules/" . $modinfo['directory'] . "/";
-		include($phpbb_root_path . "extension.inc");
-		include_once($phpbb_root_path . "includes/constants.".$phpEx);
-  		include_once($phpbb_root_path . "includes/functions.".$phpEx);
-		$langfile = $phpbb_root_path . 'pnlang/'.ZLanguage::getLanguageCodeLegacy().'/Lastposts.' . $phpEx;
-		if (!file_exists($langfile)) {
-			$phpbb_root_path . 'pnlang/eng/Lastposts.' . $phpEx;
-		}
-		include_once($langfile);
+        // include some files
+        $phpbb_root_path = "./modules/" . $modinfo['directory'] . "/";
+        include($phpbb_root_path . "extension.inc");
+        include_once($phpbb_root_path . "includes/constants.".$phpEx);
+        include_once($phpbb_root_path . "includes/functions.".$phpEx);
+        $langfile = $phpbb_root_path . 'pnlang/'.ZLanguage::getLanguageCodeLegacy().'/Lastposts.' . $phpEx;
+        if (!file_exists($langfile)) {
+            $langfile = $phpbb_root_path . 'pnlang/eng/Lastposts.' . $phpEx;
+        }
+        include_once($langfile);
 
-		$link_url = "index.php?module=" . $modinfo['url'] . "&amp;file=";
-		$blockinfo['content'] = "";
+        $link_url = "index.php?module=" . $modinfo['url'] . "&amp;file=";
+        $blockinfo['content'] = "";
 
         // Defaults some things if they are empty!
         $vars['last_X_posts'] ? ($vars['last_X_posts']) : "5";					// Show this many recent posts.
@@ -191,16 +191,16 @@ function ZphpBB2_Lastpostsblock_display($blockinfo)
     
     $content = BlockUtil::themesideblock($blockinfo);
 
-	if ($write_to_cache and !empty($content)) {
-	   // attempt to write to cache if not loaded before
-		if (!file_exists($cachedir)) {
-			mkdir($cachedir, 0777); # attempt to make the dir
-		}
-		if (!file_put_contents($cachefile, $content)) {
-			//echo "<br />Could not save data to cache. Please make sure your cache directory exists and is writable.<br />";
-		}
-	}
-	return $content;
+    if ($write_to_cache and !empty($content)) {
+       // attempt to write to cache if not loaded before
+        if (!file_exists($cachedir)) {
+            mkdir($cachedir, 0777); # attempt to make the dir
+        }
+        if (!file_put_contents($cachefile, $content)) {
+            //echo "<br />Could not save data to cache. Please make sure your cache directory exists and is writable.<br />";
+        }
+    }
+    return $content;
 }
 
 /**
@@ -208,59 +208,59 @@ function ZphpBB2_Lastpostsblock_display($blockinfo)
  */
 function ZphpBB2_Lastpostsblock_modify($blockinfo)
 {
-	// Security check
-	if (!SecurityUtil::checkPermission('ZphpBB2_Lastposts::', $blockinfo[bid]."::", ACCESS_EDIT)) {
+    // Security check
+    if (!SecurityUtil::checkPermission('ZphpBB2_Lastposts::', $blockinfo[bid]."::", ACCESS_EDIT)) {
       return false;
-	}
+    }
 
-	// get module information
-	$modinfo = ModUtil::getInfoFromName("ZphpBB2");
-	if (empty($modinfo['name']) or empty($modinfo['directory'])) {
-		return _MODULE_ERROR;
-	}
-	
-	// Get current content
-	$vars = BlockUtil::varsFromContent($blockinfo['content']);
-	$table_prefix = System::getVar('prefix') . "_phpbb_";
+    // get module information
+    $modinfo = ModUtil::getInfoFromName("ZphpBB2");
+    if (empty($modinfo['name']) or empty($modinfo['directory'])) {
+        return _MODULE_ERROR;
+    }
+    
+    // Get current content
+    $vars = BlockUtil::varsFromContent($blockinfo['content']);
+    $table_prefix = System::getVar('prefix') . "_phpbb_";
 
-	define('IN_PHPBB', true);
-	$phpbb_root_path = './modules/' . $modinfo['directory'] . '/';
-	if (file_exists($phpbb_root_path)) {
-   		include($phpbb_root_path . 'extension.inc');
-	    include_once($phpbb_root_path . 'includes/constants.'.$phpEx);
-  		include_once($phpbb_root_path . "includes/functions.".$phpEx);
-		$langfile = $phpbb_root_path . 'pnlang/'.ZLanguage::getLanguageCodeLegacy().'/Lastposts.' . $phpEx;
-		if (!file_exists($langfile)) {
-			$phpbb_root_path . 'pnlang/eng/Lastposts.' . $phpEx;
-		}
-		include_once($langfile);
-	} else {
-		return '';
-	}
-	 // Defaults
-	if (empty($vars)) {
+    define('IN_PHPBB', true);
+    $phpbb_root_path = './modules/' . $modinfo['directory'] . '/';
+    if (file_exists($phpbb_root_path)) {
+        include($phpbb_root_path . 'extension.inc');
+        include_once($phpbb_root_path . 'includes/constants.'.$phpEx);
+        include_once($phpbb_root_path . "includes/functions.".$phpEx);
+        $langfile = $phpbb_root_path . 'pnlang/'.ZLanguage::getLanguageCodeLegacy().'/Lastposts.' . $phpEx;
+        if (!file_exists($langfile)) {
+            $phpbb_root_path . 'pnlang/eng/Lastposts.' . $phpEx;
+        }
+        include_once($langfile);
+    } else {
+        return '';
+    }
+     // Defaults
+    if (empty($vars)) {
       $vars['last_X_posts'] = 			"5";					 // Show this many recent posts.
       $vars['display_date'] = "1";								 // Display date in post
       $vars['display_time'] 				 		= "1";								 // Display time in post
       $vars['group_topics'] 				 		= "0";								 // Yes=1, to show the last X topics where was posted
       $vars['display_cat_posts'] 		 		= "0";								 // Display categories with posts 
       $vars['excluded_forums'][0] 	 		= "";									 // Forums to appear in the list
-	}
-	if (empty($vars['cache_dir'])) $vars['cache_dir'] = "any_cache";
-	if (!isset($vars['cache_time'])) $vars['cache_time'] = "120";
+    }
+    if (empty($vars['cache_dir'])) $vars['cache_dir'] = "any_cache";
+    if (!isset($vars['cache_time'])) $vars['cache_time'] = "120";
 
-	if (empty($vars['excluded_forums'])) {
-		$vars['excluded_forums'] = array();
-	}
+    if (empty($vars['excluded_forums'])) {
+        $vars['excluded_forums'] = array();
+    }
 
-	// Create forum list
-	$query = "SELECT f.forum_id, f.forum_name, c.cat_title FROM " . FORUMS_TABLE . " f LEFT JOIN " . CATEGORIES_TABLE . " c ON c.cat_id = f.cat_id ORDER BY c.cat_title, f.forum_name";
-	$result = zExecuteSQLarray($query);
-	foreach ($result as $r) {
-		list($forum_id, $forum_name, $cat_title) = $r;
-		$selected = in_array($forum_id, $vars['excluded_forums']) ? 1 : 0;
-		$forums[] = array('id' => $forum_id, 'name' => $cat_title . ' / ' . $forum_name, 'selected' => $selected);
-	}
+    // Create forum list
+    $query = "SELECT f.forum_id, f.forum_name, c.cat_title FROM " . FORUMS_TABLE . " f LEFT JOIN " . CATEGORIES_TABLE . " c ON c.cat_id = f.cat_id ORDER BY c.cat_title, f.forum_name";
+    $result = zExecuteSQLarray($query);
+    foreach ($result as $r) {
+        list($forum_id, $forum_name, $cat_title) = $r;
+        $selected = in_array($forum_id, $vars['excluded_forums']) ? 1 : 0;
+        $forums[] = array('id' => $forum_id, 'name' => $cat_title . ' / ' . $forum_name, 'selected' => $selected);
+    }
 
     $renderer = Zikula_View::getInstance('ZphpBB2');
     $render_template = 'block_lastposts_modify.tpl';
@@ -276,30 +276,30 @@ function ZphpBB2_Lastpostsblock_modify($blockinfo)
 function ZphpBB2_Lastpostsblock_update($blockinfo)
 {
    list($vars['cache_time'],
-	    $vars['cache_dir'],
-		$vars['last_X_posts'],
-		$vars['last_X_scroll'],
-		$vars['scroll_height'],
-		$vars['scroll_speed'],
-		$vars['scroll_images'],
-		$vars['display_date'],
-		$vars['display_time'],
-		$vars['group_topics'],
-		$vars['display_cat_posts'], 
-		$vars['excluded_forums']
-		) = pnVarCleanFromInput(
-		'cache_time',
-		'cache_dir',
-		'last_X_posts',
-		'last_X_scroll',
-		'scroll_height',
-		'scroll_speed',
-		'scroll_images',
-		'display_date',
-		'display_time',
-		'group_topics', 
-		'display_cat_posts',
-		'excluded_forums');
+        $vars['cache_dir'],
+        $vars['last_X_posts'],
+        $vars['last_X_scroll'],
+        $vars['scroll_height'],
+        $vars['scroll_speed'],
+        $vars['scroll_images'],
+        $vars['display_date'],
+        $vars['display_time'],
+        $vars['group_topics'],
+        $vars['display_cat_posts'], 
+        $vars['excluded_forums']
+        ) = pnVarCleanFromInput(
+        'cache_time',
+        'cache_dir',
+        'last_X_posts',
+        'last_X_scroll',
+        'scroll_height',
+        'scroll_speed',
+        'scroll_images',
+        'display_date',
+        'display_time',
+        'group_topics', 
+        'display_cat_posts',
+        'excluded_forums');
    $blockinfo['content'] = pnBlockVarsToContent($vars);
 
    return $blockinfo;
--- readme.md
+++ readme.md
@@ -4,6 +4,6 @@ ZphpBB2 helps upgrading sites running PNphpBB2 from Zikula 1.2 to Zikula 1.3.
 
 ZphpBB2 does not work in Zikula 1.2. Minimum version required is Zikula 1.3.2. Upgrade is possible from PNphpBB2 1.2j or 1.2i.
 
-Working stage: module works and can be used to upgrade. Download as zip file.
+Working stage: module works and can be used to install or upgrade. Download as zip file.
 
 In tools directory are provided some export/import tools.
